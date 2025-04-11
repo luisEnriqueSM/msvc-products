8e4768aaed310b3919f71185c8c9aa219ff7315a
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-04-11 12:43:48 -0600

    Add logs on ProductController for Micrometer

--- src/main/java/com/luis/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/luis/springcloud/msvc/products/controllers/ProductController.java
@@ -1,8 +1,9 @@
 package com.luis.springcloud.msvc.products.controllers;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +20,8 @@ import com.luis.springcloud.msvc.products.services.ProductService;
 @RestController
 public class ProductController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     final private ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -27,11 +30,14 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<?> list(){
+        logger.info("Ingresando al metodo del controller ProductController::list");
         return ResponseEntity.ok(this.productService.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> details(@PathVariable Long id){
+        logger.info("Ingresando al metodo del controller ProductController::details");
+
         Optional<Product> productOptional = this.productService.findById(id);
 
         if(productOptional.isPresent()){
@@ -42,11 +48,13 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> create(@RequestBody Product product){
+        logger.info("Ingresando al metodo del controller ProductController::create, creando: {}", product);
         return ResponseEntity.status(HttpStatus.CREATED).body(this.productService.save(product));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Product product){
+        logger.info("Ingresando al metodo del controller ProductController::update, actualizando: {}", product);
         Optional<Product> productOptional = this.productService.findById(id);
         if(productOptional.isPresent()){
             Product productDb = productOptional.orElseThrow();
@@ -60,9 +68,11 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
+        
         Optional<Product> productOptional = this.productService.findById(id);
         if(productOptional.isPresent()){
             this.productService.deleteById(id);
+            logger.info("Ingresando al metodo del controller ProductController::delete, eliminando: {}", productOptional.get());
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
